2cf5ab3f17e2ce3a2b6950834b4fd302d2481ef3
陈随易 <chenbimo@gmail.com>
2024-12-14 18:30:54 +0800

    删除多余的文件和目录

--- .mocharc.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  exit: true,
-  timeout: '0'
-};
--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,20 @@
 const errorCode = {
-  generic: 'ERR_GENERIC_CLIENT',
-  connect: 'ERR_NOT_CONNECTED',
-  badPath: 'ERR_BAD_PATH',
-  permission: 'EACCES',
-  notexist: 'ENOENT',
-  notdir: 'ENOTDIR',
-  badAuth: 'ERR_BAD_AUTH',
+    generic: 'ERR_GENERIC_CLIENT',
+    connect: 'ERR_NOT_CONNECTED',
+    badPath: 'ERR_BAD_PATH',
+    permission: 'EACCES',
+    notexist: 'ENOENT',
+    notdir: 'ENOTDIR',
+    badAuth: 'ERR_BAD_AUTH'
 };
 
 const targetType = {
-  writeFile: 1,
-  readFile: 2,
-  writeDir: 3,
-  readDir: 4,
-  readObj: 5,
-  writeObj: 6,
+    writeFile: 1,
+    readFile: 2,
+    writeDir: 3,
+    readDir: 4,
+    readObj: 5,
+    writeObj: 6
 };
 
-module.exports = {
-  errorCode,
-  targetType,
-};
+export { errorCode, targetType };
